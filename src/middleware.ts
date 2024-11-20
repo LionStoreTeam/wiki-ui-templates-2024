10e1f39c116b10eb971ed9041dfce62506af51f3
LionStoreTeam <lionstoretechnologyteam@gmail.com>
2024-11-20 17:28:49 -0600

    Wiki UI Templates 2024

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+// import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
 export { default } from "next-auth/middleware";
 
 // export default withAuth(
@@ -22,11 +22,11 @@ export { default } from "next-auth/middleware";
 //   }
 // );
 // ----------------------------------------------------------------
-export function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname.startsWith("/auth/login?callbackUrl=")) {
-    return NextResponse.rewrite(new URL("/auth/login", request.url));
-  }
-}
+// export function middleware(request: NextRequest) {
+//   if (request.nextUrl.pathname.startsWith("/dashboard")) {
+//     return NextResponse.rewrite(new URL("/dashboard/user", request.url));
+//   }
+// }
 
 // Configuración de las rutas protegidas
 export const config = {
