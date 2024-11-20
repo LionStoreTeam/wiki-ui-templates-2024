9d6f964a17af0ca4db7bcbc8d40a6153063f7773
LionStoreTeam <lionstoretechnologyteam@gmail.com>
2024-11-20 00:21:28 -0600

    Wiki UI Templates 2024

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -44,20 +44,20 @@ export default function LoginPage() {
     });
 
     return (
-        <div className="">
-            {/* Sección del Formulario Inicio de Sesión*/}
-            <div className="min-h-screen flex flex-col justify-center sm:py-12 bg-slate-100">
-                <div className="flex justify-center items-center text-center my-5">
-                    <Link href="/">
-                        <div className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-gray-900 rounded-3xl group bg-gradient-to-br from-red-500 to-red-500 group-hover:from-red-500 group-red:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800 text-[15px] sm:text-[35px] md:text-[40px]">
-                            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-[#333] rounded-3xl group-hover:bg-opacity-0">
-                                Volver a la pantalla de Inicio
-                            </span>
-                        </div>
-                    </Link>
-                </div>
-                {/*  */}
-                <Suspense fallback={<div>Loading...</div>}>
+        <Suspense fallback={<div>Loading...</div>}>
+            <div className="">
+                {/* Sección del Formulario Inicio de Sesión*/}
+                <div className="min-h-screen flex flex-col justify-center sm:py-12 bg-slate-100">
+                    <div className="flex justify-center items-center text-center my-5">
+                        <Link href="/">
+                            <div className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-gray-900 rounded-3xl group bg-gradient-to-br from-red-500 to-red-500 group-hover:from-red-500 group-red:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800 text-[15px] sm:text-[35px] md:text-[40px]">
+                                <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-[#333] rounded-3xl group-hover:bg-opacity-0">
+                                    Volver a la pantalla de Inicio
+                                </span>
+                            </div>
+                        </Link>
+                    </div>
+                    {/*  */}
                     <div className="relative z-20 mx-auto max-w-7xl px-6 lg:px-8 ">
                         <div className="mx-auto max-w-2xl text-center">
                             <div className="shadow-inner py-5">
@@ -148,86 +148,86 @@ export default function LoginPage() {
                             </div>
                         </div>
                     </div>
-                </Suspense>
 
-                {/*  */}
-                <div className="flex h-screen items-center justify-center p-10">
-                    <div className="xl:w-1/2  rounded-2xl border border-red-500 md:shadow-xl">
-                        <div className="grid md:grid-cols-2 p-5">
-                            <div className="">
-                                <img
-                                    src="https://cdni.iconscout.com/illustration/premium/thumb/login-10299071-8333958.png?f=webp"
-                                    alt=""
-                                />
-                            </div>
-                            <div className="flex flex-col items-center justify-center">
-                                <form onSubmit={onSubmit}>
-                                    {error && (
-                                        <p className="bg-red-500 text-sm font-bold text-slate-100 p-3  mt-3 rounded-xl">
-                                            {error}
-                                        </p>
-                                    )}
-                                    <h1 className="text-center font-extrabold uppercase text-rose-500">
-                                        Inicio de Sesión
-                                    </h1>
-                                    <br />
-                                    <input
-                                        {...register("email", {
-                                            required: {
-                                                value: true,
-                                                message: "El correo es requerido",
-                                            },
-                                        })}
-                                        autoComplete="off"
-                                        id="email"
-                                        name="email"
-                                        type="email"
-                                        className="mb-3 w-full rounded-2xl bg-zinc-100 outline-rose-400 px-5 py-3"
-                                        placeholder="correo@gmail.com"
+                    {/*  */}
+                    <div className="flex h-screen items-center justify-center p-10">
+                        <div className="xl:w-1/2  rounded-2xl border border-red-500 md:shadow-xl">
+                            <div className="grid md:grid-cols-2 p-5">
+                                <div className="">
+                                    <img
+                                        src="https://cdni.iconscout.com/illustration/premium/thumb/login-10299071-8333958.png?f=webp"
+                                        alt=""
                                     />
-                                    {errors.email && (
-                                        <span className="text-red-500">
-                                            {errors.email.message?.toString()}
-                                        </span>
-                                    )}
-                                    <input
-                                        {...register("password", {
-                                            required: {
-                                                value: true,
-                                                message: "La contraseña es requerida",
-                                            },
-                                        })}
-                                        autoComplete="off"
-                                        id="password"
-                                        name="password"
-                                        type="password"
-                                        className="mb-3 w-full rounded-2xl bg-zinc-100 outline-rose-400 px-5 py-3"
-                                        placeholder="••••••••••"
-                                    />
-                                    {errors.password && (
-                                        <span className="text-red-500">
-                                            {errors.password.message?.toString()}
-                                        </span>
-                                    )}
-                                    <button
-                                        type="submit"
-                                        className="mb-3 w-full rounded-2xl bg-rose-500 px-5 py-3 font-semibold text-white"
-                                    >
-                                        Ingresar
-                                    </button>
-                                </form>
-                                <Link href="/auth/signup">
-                                    <div className="py-2 px-4 bg-slate-300 text-pink-500 font-bold rounded-xl">
-                                        Crear una cuenta
-                                    </div>
-                                </Link>
+                                </div>
+                                <div className="flex flex-col items-center justify-center">
+                                    <form onSubmit={onSubmit}>
+                                        {error && (
+                                            <p className="bg-red-500 text-sm font-bold text-slate-100 p-3  mt-3 rounded-xl">
+                                                {error}
+                                            </p>
+                                        )}
+                                        <h1 className="text-center font-extrabold uppercase text-rose-500">
+                                            Inicio de Sesión
+                                        </h1>
+                                        <br />
+                                        <input
+                                            {...register("email", {
+                                                required: {
+                                                    value: true,
+                                                    message: "El correo es requerido",
+                                                },
+                                            })}
+                                            autoComplete="off"
+                                            id="email"
+                                            name="email"
+                                            type="email"
+                                            className="mb-3 w-full rounded-2xl bg-zinc-100 outline-rose-400 px-5 py-3"
+                                            placeholder="correo@gmail.com"
+                                        />
+                                        {errors.email && (
+                                            <span className="text-red-500">
+                                                {errors.email.message?.toString()}
+                                            </span>
+                                        )}
+                                        <input
+                                            {...register("password", {
+                                                required: {
+                                                    value: true,
+                                                    message: "La contraseña es requerida",
+                                                },
+                                            })}
+                                            autoComplete="off"
+                                            id="password"
+                                            name="password"
+                                            type="password"
+                                            className="mb-3 w-full rounded-2xl bg-zinc-100 outline-rose-400 px-5 py-3"
+                                            placeholder="••••••••••"
+                                        />
+                                        {errors.password && (
+                                            <span className="text-red-500">
+                                                {errors.password.message?.toString()}
+                                            </span>
+                                        )}
+                                        <button
+                                            type="submit"
+                                            className="mb-3 w-full rounded-2xl bg-rose-500 px-5 py-3 font-semibold text-white"
+                                        >
+                                            Ingresar
+                                        </button>
+                                    </form>
+                                    <Link href="/auth/signup">
+                                        <div className="py-2 px-4 bg-slate-300 text-pink-500 font-bold rounded-xl">
+                                            Crear una cuenta
+                                        </div>
+                                    </Link>
+                                </div>
+                                <Toaster />
                             </div>
-                            <Toaster />
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </Suspense>
     );
 }
 
